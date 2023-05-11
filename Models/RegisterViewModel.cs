9bd26415d5def9a4a9e459b06abfd51bd8750290
vule2303 <leductuanvu5@gmail.com>
2023-05-11 21:29:48 +0700

    rewrite register auto login api

--- Models/RegisterViewModel.cs
+++ Models/RegisterViewModel.cs
@@ -1,9 +1,21 @@
-﻿namespace BookLibraryAPI.Models
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookLibraryAPI.Models
 {
     public class RegisterViewModel
     {
+        [System.ComponentModel.DataAnnotations.Required]
+        [DataType(DataType.Text)]
         public string UserName { get; set; }
-        public string Password { get; set; }
+        [System.ComponentModel.DataAnnotations.Required]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
+        [System.ComponentModel.DataAnnotations.Required]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+        [System.ComponentModel.DataAnnotations.Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
