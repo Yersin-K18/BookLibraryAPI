1f2d26610a71d631954d0ac258e6ab75c997ec43
phu <phu3365@gmail,com>
2023-06-25 21:05:20 +0700

    dont search theo author

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,29 @@
+﻿using BookLibraryAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookLibraryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly BooklibraryContext _context;
+
+        public SearchController(BooklibraryContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("products")]
+        public IActionResult GetProductsByAuthor(string author)
+        {
+            var searchResults = _context.Products
+           .Where(p => p.Author.Name.Contains(author) || p.Author.Nickname.Contains(author))
+            .ToList();
+            if (searchResults.Count == 0) {
+                return NotFound("Không tìm thấy sản phẩm ");
+            }
+            return Ok(searchResults);
+        }
+    }
+}
