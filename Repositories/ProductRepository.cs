04b5610ea1ee03389fb24bfb4019177b74b2c68d
HoangDSR <nvh.02021995@gmail.com>
2023-06-25 13:54:13 +0700

    fix BooklibraryContext

--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -1,18 +1,17 @@
-﻿using BookLibraryAPI.Models.DTO;
+﻿using BookLibraryAPI.Data;
 using BookLibraryAPI.Models;
-using System;
-using Microsoft.EntityFrameworkCore;
+using BookLibraryAPI.Models.DTO;
 
 namespace BookLibraryAPI.Repositories
 {
     public class ProductRepository : IProductRepository
     {
-        
-            private readonly BooklibraryContext _context;
-            public ProductRepository(BooklibraryContext dbContext)
-            {
-                _context = dbContext;
-            }
+
+        private readonly BooklibraryContext _context;
+        public ProductRepository(BooklibraryContext dbContext)
+        {
+            _context = dbContext;
+        }
         public AddProductDTO AddProduct(AddProductDTO addProductDTO)
         {
             var productmodels = new Product
@@ -25,80 +24,80 @@ namespace BookLibraryAPI.Repositories
                 CategorieId = addProductDTO.CategorieId,
                 //Use Domain Model to add Book
             };
-                _context.Products.Add(productmodels);
-                _context.SaveChanges();
+            _context.Products.Add(productmodels);
+            _context.SaveChanges();
             return addProductDTO;
         }
 
         public Product? DeleteProductById(int id)
+        {
+            var bookDomain = _context.Products.FirstOrDefault(n => n.Id == id);
+            if (bookDomain != null)
             {
-                var bookDomain = _context.Products.FirstOrDefault(n => n.Id == id);
-                if (bookDomain != null)
-                {
-                    _context.Products.Remove(bookDomain);
-                    _context.SaveChanges();
-                }
-                return bookDomain;
-
+                _context.Products.Remove(bookDomain);
+                _context.SaveChanges();
             }
+            return bookDomain;
 
-            public List<ProductDTO> GetAllProduct()
+        }
+
+        public List<ProductDTO> GetAllProduct()
+        {
+            var AllProduct = _context.Products.Select(product => new ProductDTO()
             {
-                var AllProduct = _context.Products.Select(product => new ProductDTO()
-                {
-                    Id = product.Id,
-                    Name = product.Name,
-                    Description = product.Description,
-                    Price = product.Price,
-                    Language = product.Language,
-                    Image = product.Image,
-                    Quantity = product.Quantity,
-                    AuthorName = product.Author.Name,
-                    CategoriesName = product.Categorie.Name,
-                }).ToList();
-                return AllProduct;
+                Id = product.Id,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                Language = product.Language,
+                Image = product.Image,
+                Quantity = product.Quantity,
+                AuthorName = product.Author.Name,
+                CategoriesName = product.Categorie.Name,
+            }).ToList();
+            return AllProduct;
+        }
+
+        public ProductDTO GetProductById(int id)
+        {
+            var productwithModels = _context.Products.Where(n => n.Id == id);
+            //Map Domain Model to DTOs
+            var productwithDTO = productwithModels.Select(product => new
+           ProductDTO()
+            {
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price,
+                Language = product.Language,
+                Image = product.Image,
+                Quantity = product.Quantity,
+                AuthorName = product.Author.Name,
+                CategoriesName = product.Categorie.Name,
+            }).FirstOrDefault();
+            return productwithDTO;
+        }
+
+        public AddProductDTO? UpdateProductById(int id, AddProductDTO productDTO)
+        {
+            var ProductModels = _context.Products.FirstOrDefault(n => n.Id == id);
+            if (ProductModels != null)
+            {
+                ProductModels.Name = productDTO.Name;
+                ProductModels.Description = productDTO.Description;
+                ProductModels.Price = productDTO.Price;
+                ProductModels.Language = productDTO.Language;
+                ProductModels.Image = productDTO.Image;
+                ProductModels.Quantity = productDTO.Quantity;
+                ProductModels.AuthorId = productDTO.AuthorId;
+                ProductModels.CategorieId = productDTO.CategorieId;
+                _context.SaveChanges();
             }
-
-            public ProductDTO GetProductById(int id)
-            {
-                var productwithModels = _context.Products.Where(n => n.Id == id);
-                //Map Domain Model to DTOs
-                var productwithDTO = productwithModels.Select(product => new
-               ProductDTO()
-                {
-                    Name = product.Name,
-                    Description = product.Description,
-                    Price = product.Price,
-                    Language = product.Language,
-                    Image = product.Image,
-                    Quantity = product.Quantity,
-                    AuthorName = product.Author.Name,
-                    CategoriesName = product.Categorie.Name,
-                }).FirstOrDefault();
-                return productwithDTO;
-            }
-
-            public AddProductDTO? UpdateProductById(int id, AddProductDTO productDTO)
-            {
-                var ProductModels = _context.Products.FirstOrDefault(n => n.Id == id);
-                if (ProductModels != null)
-                {
-                    ProductModels.Name = productDTO.Name;
-                    ProductModels.Description = productDTO.Description;
-                    ProductModels.Price = productDTO.Price;
-                    ProductModels.Language = productDTO.Language;
-                    ProductModels.Image = productDTO.Image;
-                    ProductModels.Quantity = productDTO.Quantity;
-                    ProductModels.AuthorId = productDTO.AuthorId;
-                    ProductModels.CategorieId = productDTO.CategorieId;
-                    _context.SaveChanges();
-                }
             else
             {
                 Console.WriteLine("chưa cập nhật thành công");
             }
-                return productDTO;
+            return productDTO;
 
-            }
         }
     }
+}
