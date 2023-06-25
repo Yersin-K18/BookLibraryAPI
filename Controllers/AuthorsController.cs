774542ca88cb873ec068cb968dd8f6563551e03f
phu <phu3365@gmail,com>
2023-06-25 13:29:21 +0700

    sửa

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -15,39 +15,39 @@ namespace BookLibraryAPI.Controllers
         private readonly BooklibraryContext _context;
         private readonly IAuthorRepository _authorRepository;
 
-        public AuthorsController(BooklibraryContext dbContext,IAuthorRepository
+        public AuthorsController(BooklibraryContext dbContext, IAuthorRepository
        authorRepository)
         {
             _context = dbContext;
             _authorRepository = authorRepository;
         }
-        [HttpGet("get-all-Authors")]
+        [HttpGet()]
         public IActionResult GetAllUser()
         {
             var allAuthor = _authorRepository.GetAllAuthor();
             return Ok(allAuthor);
         }
-        [HttpGet("get-authors-by-id/{id}")]
+        [HttpGet("{id}")]
         public IActionResult GetAuthorById(int id)
         {
             var AuthorbyID = _authorRepository.GetAuthorById(id);
             return Ok(AuthorbyID);
         }
-        [HttpPost("add-Author")]
+        [HttpPost()]
         public IActionResult AddUser([FromBody] AddAuthorDTO
        addAuthorDTO)
         {
             var addAuthor = _authorRepository.AddAuthor(addAuthorDTO);
             return Ok();
         }
-        [HttpPut("update-Author-by-id/{id}")]
+        [HttpPut("{id}")]
         public IActionResult UpdateAuthorById(int id, [FromBody] AuthorNoIdDTO
        authorDTO)
         {
             var authorUpdate = _authorRepository.UpdateAuthor(id, authorDTO);
             return Ok(authorUpdate);
         }
-        [HttpDelete("delete-author-by-id/{id}")]
+        [HttpDelete("{id}")]
         public IActionResult DeleteAuthorById(int id)
         {
             var userDelete = _authorRepository.DeleteAuthorById(id);
