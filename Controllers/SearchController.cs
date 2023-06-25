d65bc532149a9947c3c788738ab6ee535c42352e
phu <phu3365@gmail,com>
2023-06-25 20:39:10 +0700

    tìm kiếm theo tên sản phẩm (done)

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,31 @@
+﻿using BookLibraryAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookLibraryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly BooklibraryContext _context;
+
+        public SearchController(BooklibraryContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("searchProduct")]
+        public IActionResult searchPruduct(string keyword)
+        {
+            var searchResults = _context.Products
+            .Where(a => !(!a.Name.Contains(keyword)))
+            .ToList();
+            if ( searchResults.Count == 0)
+            {
+                return NotFound("không tìm thấy sản phẩm");
+            }    
+            return Ok(searchResults);
+        }
+        
+    }
+}
